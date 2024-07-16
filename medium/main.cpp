39f4c36382f87cc81c629b9dff3383db7a3d5e91
C3mR4de <nvadim2005@gmail.com>
2024-07-16 21:15:05 +0300

    Added medium difficulty task

---
+++ medium/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
+int main()
+{
+    std::ifstream file("17.txt");
+
+    std::vector<int> nums;
+    nums.reserve(100000);
+
+    using input_it = std::istream_iterator<int>;
+    std::copy(input_it(file), input_it(), std::back_inserter(nums));
+
+    std::size_t count = 0;
+    std::size_t min = 100000;
+
+    for (std::size_t i = 0; i < 100000; ++i)
+    {
+        for (std::size_t j = i + 1; j < 100000; ++j)
+        {
+            if ((nums[i] + nums[j]) % 10 == 9)
+            {
+                ++count;
+                min = std::min<std::size_t>(min, nums[i] + nums[j]);
+            }
+        }
+    }
+
+    std::cout << count << ' ' << min << '\n';
+
+    return 0;
+}
